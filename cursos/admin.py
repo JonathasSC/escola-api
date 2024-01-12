fd29d69ec765ae5bcd732c7e7d041beec7fd2c7c
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-01-12 01:04:04 -0300

    created models

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Curso, Avaliacao
+
 
 # Register your models here.
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo')
+
+
+@admin.register(Avaliacao)
+class Avaliacao(admin.ModelAdmin):
+    list_display = ('curso',
+                    'nome',
+                    'email',
+                    'avaliacao',
+                    'criacao',
+                    'atualizacao',
+                    'ativo')
